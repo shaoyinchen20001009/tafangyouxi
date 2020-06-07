bde1b1af7ea61de6ebb23219baa97b409c77dc83
shaoyinchen20001009 <66291670+shaoyinchen20001009@users.noreply.github.com>
2020-06-07 12:15:36 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "mainwindow1.h"
+#include <QApplication>
+#include<QString>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow1 w;
+    w.show();
+
+    return a.exec();
+}
